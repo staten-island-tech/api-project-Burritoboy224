c01b89118c601718b49621ed7db73037b3f17f08
Yuhan224 <fir3panic@gmail.com>
2023-12-07 09:22:52 -0500

    guy

--- api-project/main.js
+++ api-project/main.js
@@ -1,2 +1,11 @@
 import './style.css'
 
+const URL = "https://pokeapi.co/api/v2/pokemon/" 
+try {
+    const response = await fetch (URL);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.content;
+} catch (error) {
+    console.log(error);
+}
+getData (URL);
