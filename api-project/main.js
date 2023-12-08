5d8dd3da7eaa8e367381eb310988cf4b6c9123e5
Yuhan224 <fir3panic@gmail.com>
2023-12-08 09:24:18 -0500

    ASDF

--- api-project/main.js
+++ api-project/main.js
@@ -1,10 +1,15 @@
 import './style.css'
 
-const URL = "https://pokeapi.co/api/v2/pokemon/" 
+const URL = "https://pokeapi.co/api/v2/pokemon" 
 try {
     const response = await fetch (URL);
+    if (response.status !=200){
+        throw new error(response.statusText);
+    }
+
     const data = await response.json();
     document.getElementById("api-response").textContent = data.content;
+    console.log(data)
 } catch (error) {
     console.log(error);
 }
