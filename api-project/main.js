7cd76ad814723c04408c47b2aee4c43e7d1c25a6
Yuhan224 <fir3panic@gmail.com>
2024-01-10 08:49:05 -0500

    f

--- api-project/main.js
+++ api-project/main.js
@@ -20,13 +20,54 @@ async function getData(URL) {
         }
 
         async function insertPokemon(pokemonList) {
-            pokemonList.forEach((pokemon) => document.querySelector(".cards").insertAdjacentHTML("beforeend",
-                `<div class="card">
-                    <h2 class ="description">${pokemon.name}</h2>
-                    <img class ="image" src="${pokemon.image}" alt="${pokemon.name}"><br>
-                </div>`
-            ));
+            const cardsContainer = document.querySelector(".cards");
+        
+            pokemonList.forEach((pokemon) => {
+                const card = document.createElement("div");
+                card.classList.add("card");
+        
+                const description = document.createElement("h2");
+                description.classList.add("description");
+                description.textContent = pokemon.name;
+        
+                const image = document.createElement("img");
+                image.classList.add("image");
+                image.src = pokemon.image;
+                image.alt = pokemon.name;
+        
+                // Add event listener to each card
+                card.addEventListener("click", async() => {
+                    const response2 =  await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+                    const response3 = await response2.json();
+                    console.log(response3);
+                    await document.querySelector('body').insertAdjacentHTML(
+                        "beforeend",
+                        `<dialog class="cardsssssssssssss">
+                        <h2 class = "text">${pokemon.name}</h2>
+                        <img src="${pokemon.image}" alt="">
+                        <h2 class = "text"> HP: ${response3.stats[0].base_stat}</h2>
+                        <h2 class = "text"> ATK: ${response3.stats[1].base_stat}</h2>
+                        <h2 class = "text"> DEF: ${response3.stats[2].base_stat}</h2>
+                        <h2 class = "text"> SP-ATK: ${response3.stats[3].base_stat}</h2>
+                        <h2 class = "text"> SP-DEF: ${response3.stats[4].base_stat}</h2>
+                        <h2 class = "text"> SPD: ${response3.stats[5].base_stat}</h2>
+                          <button alt = "click escape to return" id="close" class="button-1" autofocus> click to return </button>
+                        </dialog>`
+                        );
+                        document.querySelector("dialog").showModal();
+                        document.getElementById("close").addEventListener("click",function(){
+                            this.parentElement.remove();
+                          })
+                    console.log(`Clicked on ${pokemon.name} card`); 
+                });
+        
+                card.appendChild(description);
+                card.appendChild(image);
+        
+                cardsContainer.appendChild(card);
+            });
         }
+
         
        
 
